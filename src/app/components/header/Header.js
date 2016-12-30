ee645daaf5292c19ea0a97d78c77701a144c314f
Victor Igor <victor.0w3@hotmail.com>
2016-12-30 18:02:13 -0200

    added one connected OK and placeload on repository

--- src/app/components/header/Header.js
+++ src/app/components/header/Header.js
@@ -1,8 +1,14 @@
 class HeaderController {
     constructor($scope, onlineStatus){
         $scope.onlineStatus = onlineStatus;
+        $scope.online_again = false;
+        $scope.on = false;
         $scope.$watch('onlineStatus.isOnline()', function(online) {
             $scope.online_status = online ? true : false;
+            $scope.online_again = $scope.online_status === false ? true: false;
+            if($scope.online_again){
+                $scope.on = true;
+            }
         });
     }
 
