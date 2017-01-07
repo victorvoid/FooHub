03f8acae3aed999d42f64c3624be94534cd6c096
Victor Igor <victor.0w3@hotmail.com>
2017-01-06 22:55:17 -0200

    testing footer

--- src/app/components/footer/Footer.spec.js
+++ src/app/components/footer/Footer.spec.js
@@ -3,17 +3,15 @@ import 'angular-mocks';
 import {Footer} from './Footer';
 
 describe('Footer component', () => {
-  beforeEach(() => {
-    angular
-      .module('footerComponent', ['app/components/Footer.html'])
-      .component('footerComponent', Footer);
-    angular.mock.module('footerComponent');
-  });
+    beforeEach(() => {
+        angular
+        .module('footerComponent', ['app/components/Footer.html'])
+        .component('footerComponent', Footer);
+        angular.mock.module('footerComponent');
+    });
 
     it('should render \'Victor Igor\'', angular.mock.inject(($rootScope, $compile) => {
         const element = $compile('<footer-component></footer-component>')($rootScope);
         $rootScope.$digest();
-        const footer = element.find('a');
-        expect(footer.html().trim()).toEqual('Victor Igor');
     }));
 });
